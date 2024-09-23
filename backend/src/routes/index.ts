eaa0096c938b858635293e3deed939847858a402
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2024-09-23 16:23:31 -0300

    feat: rota da classe saída

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import entradaRoutes from './entradaRoutes'
 import produtoRoutes from './produtoRoutes';
 import loteRoutes from './loteRoutes';
+import saidaRoutes from './saidaRoutes'
 
 const router = Router();
 
 router.use('/entrada', entradaRoutes)
 router.use('/produto', produtoRoutes)
 router.use('/lote', loteRoutes)
+router.use('/saida', saidaRoutes)
 
 export default router;
