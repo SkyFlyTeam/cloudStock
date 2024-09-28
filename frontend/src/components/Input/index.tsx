f05d9912df9207afb762437a225e4852eb52252d
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-09-28 10:20:32 -0300

    feat: adicionando component input

---
+++ frontend/src/components/Input/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'; 
+import './style.css'; 
+
+interface InputProps {
+  label: string; // Rótulo do input
+  placeholder?: string; // Texto que aparece quando o input está vazio
+  type?: string; // Tipo do input, por padrão é "text"
+  value?: string; // Valor do input
+  options?: string[]; // Nova prop para as opções do select, se for o caso
+  onChange?: (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void; // Função de callback para lidar com mudanças
+}
+
+
+const Input: React.FC<InputProps> = ({ label, placeholder, type = "text", value, options, onChange }) => {
+  return (
+    <div>
+
+      <label>{label}</label> 
+      
+      {/* Condição que verifica se o tipo do input é "select" */}
+      {type === "select" ? (
+        <select className="input-field" value={value} onChange={onChange}> {/* Se for "select", renderiza um select */}
+          <option value="">{placeholder || "Selecione..."}</option> 
+          {options && options.map((option, index) => ( // Se existem opções, mapeia cada uma para criar uma option
+            <option key={index} value={option}>{option}</option> // Cada opção recebe um valor e exibe o texto correspondente
+          ))}
+        </select>
+      ) : (
+        <input
+          type={type}
+          placeholder={placeholder} 
+          value={value}
+          onChange={onChange} 
+          className="input-field"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Input; 
