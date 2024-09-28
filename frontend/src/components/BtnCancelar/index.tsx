45759281721236be77fa2baf2f603f76e55bb309
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-09-28 00:41:06 -0300

    fix: adição props e onClick no botão de cancelar

--- frontend/src/components/BtnCancelar/index.tsx
+++ frontend/src/components/BtnCancelar/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './style.css';
 
-const BtnCancelar: React.FC = () => {
+interface BtnCancelarProps {
+    onClick: () => void; // Recebe a função para abrir/fechar o modal
+}
+
+const BtnCancelar: React.FC<BtnCancelarProps> = ({ onClick }) => {
     return (
-        <button className='btn-cancelar'>
+        <button className='btn-cancelar' onClick={onClick}>
             CANCELAR
         </button>
     );
