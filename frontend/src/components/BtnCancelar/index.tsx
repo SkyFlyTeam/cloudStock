208a1def61d1be617f98d5edfbc3c382ea75c629
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-09-28 00:36:12 -0300

    feat: adicão componente botão cancelar

---
+++ frontend/src/components/BtnCancelar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const BtnCancelar: React.FC = () => {
+    return (
+        <button className='btn-cancelar'>
+            CANCELAR
+        </button>
+    );
+}
+
+export default BtnCancelar;
