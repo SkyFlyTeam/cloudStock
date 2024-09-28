258e48726a2da0041dd33e0cfa00e5f918d3e141
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-09-28 10:26:17 -0300

    refactor: reorganizar estrutura da tela

--- frontend/src/pages/LocalArmazenamento/index.tsx
+++ frontend/src/pages/LocalArmazenamento/index.tsx
@@ -1,8 +1,47 @@
 import React, { useState, useEffect } from "react";
+import InputBusca from "../../components/InputBusca";
+import { IoAddCircleOutline } from "react-icons/io5";
+import BtnAzul from "../../components/BtnAzul";
+import './style.css'
+import '../../style/global.css'
+import Modal from "../../components/Modal";
+import Input from "../../components/Input";
 
 function LocalArmazenamento() {
+    const [openModal, setOpenModal] = useState(false)
+    const [name, setName] = useState('')
+    const [sector, setSector] = useState('')
+
     return (
-        <h1>Local Armazenamento</h1>
+        <>
+            <h1 className="title">Local Armazenamento</h1>
+
+            <div className="inputButton">
+                <InputBusca />
+                <BtnAzul icon={<IoAddCircleOutline />} label='CADASTRAR' onClick={() => setOpenModal(true)} />
+            </div>
+
+            <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)} label='Cadastrar local'>
+                <div className="inputEspaçamento">
+                    <Input
+                        label="Nome"
+                        placeholder="Razão social"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <Input
+                        label="Setor"
+                        type="select"
+                        value={sector}
+                        placeholder="Selecione..."
+                        options={['Tecnologia', 'Financeiro', 'Marketing']} /* select com testes */
+                        onChange={(e) => setSector(e.target.value)}
+                    />
+                </div>
+            </Modal>
+
+        </>
+
     )
 }
 
