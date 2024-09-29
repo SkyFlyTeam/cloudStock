d8b26e0e4386aa3ca71179f43042ce3890d4f0fd
Arthur Johannes <arthur.peres@fatec.sp.gov.br>
2024-09-29 19:13:36 -0300

    feat: Implementação de 'Produtos' com função de 'Cadastro' em 'Modal'
    remove: Remoção de página de teste '/Cadastrar'

--- frontend/src/pages/Produtos/index.tsx
+++ frontend/src/pages/Produtos/index.tsx
@@ -1,18 +1,24 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 import { Table } from "react-bootstrap"
 import { ColumnDef, createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
 
-import { produtosServices } from "../../services/produtoServices"
-import { Produto } from "../../services/produtoServices"
+import { produtoServices, Produto } from "../../services/produtoServices"
 import { ApiException } from "../../config/apiException"
 
 import ImagemProduto from "../../components/ImagemProduto/index"
 import ToggleBtn from "../../components/ToggleBtn/index"
 import EditarRemoverBtn from "../../components/EditarRemoverBtn"
 
-import './style.css'
+import BtnAzul from "../../components/BtnAzul";
+import Modal from "../../components/Modal";
+import BtnCancelar from "../../components/BtnCancelar";
+import ProdutoFormulario from "../../components/Formularios/Produtos/Form_Cadastrar";
 
+import './style.css'
+/* Icons */
+import { IoAddCircleOutline } from "react-icons/io5"
+import { AiOutlineDelete } from "react-icons/ai"
 
 // Criar o helper para colunas
 const columnHelper = createColumnHelper<Produto>()
@@ -21,19 +27,31 @@ function Produtos() {
   // Use state para armazenar uma array de Produto (interface) que será exibido na tabela
   const [data, setData] = useState<Produto[]>([])
 
-  // Chama a função para pegar todos os produtos do BD
-  useEffect(() => {
-    const fetchProdutos = async () => {
-      const result = await produtosServices.getAllProdutos()
-      if (result instanceof ApiException) {
-        alert(result.message) //caso der erro, exibe a mensagem de erro num alert
-      } else {
-        setData(result) // caso der certo, armazena os dados no useState data
-      }
-    };
+  // Estado para controlar os modais
+  const [openModalCadastro, setOpenModalCadastro] = useState(false);
 
+  // Mensagem de sucesso das ações
+  const [mensagemSucesso, setMensagemSucesso] = useState<string>('');
+
+  // Referência ao forms para realizar o submit fora do componente do forms
+  const formRef = useRef<{ submitForm: () => void }>(null);
+
+
+  // Função para buscar todos os produtos
+  const fetchProdutos = async () => {
+    const result = await produtoServices.getAllProdutos()
+    if (result instanceof ApiException) {
+      console.log(result.message)
+    } else {
+      setData(result);
+    }
+  }
+
+  // Chama a função para pegar todos os produtos do BD ao montar o componente
+  useEffect(() => {
     fetchProdutos()
   }, [])
+
 
   // Função para atualizar o status do produto usando o toggle button !aqui é apenas para atualizar localmente (o useState) !
   const handleStatusChange = (prod_cod: number, newStatus: boolean) => {
@@ -44,53 +62,53 @@ function Produtos() {
     )
   }
 
-    // Colunas da tabela
-    const columns: ColumnDef<Produto, any>[] = [
-        columnHelper.display({
-            id: 'table-img',
-            cell: props => <ImagemProduto prod_cod={props.row.original.Prod_cod} />,
-        }),
-        columnHelper.accessor('Prod_nome', {
-            header: () => 'Nome',
-            cell: info => info.getValue(),
-        }),
-        columnHelper.accessor('Categoria', {
-            header: () => 'Categoria',
-            cell: info => `${info.getValue()}`, 
-        }),
-        // columnHelper.accessor('prod_quantidade', {
-        //     header: () => 'Quantidade',
-        //     cell: info => `${info.getValue()}`, 
-        // }), Comentado pois no momento não temos essa informação
+  // Colunas da tabela
+  const columns: ColumnDef<Produto, any>[] = [
+    columnHelper.display({
+      id: 'table-img',
+      cell: props => <ImagemProduto prod_cod={props.row.original.Prod_cod} />,
+    }),
+    columnHelper.accessor('Prod_nome', {
+      header: () => 'Nome',
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor('Categoria', {
+      header: () => 'Categoria',
+      cell: info => `${info.getValue()}`,
+    }),
+    columnHelper.accessor('Prod_quantidade', {
+      header: () => 'Quantidade',
+      cell: info => `${info.getValue()}`,
+    }),/* Comentado pois no momento não temos essa informação
         columnHelper.accessor('Prod_quantidade', {
             header: () => 'Quantidade',
             cell: info => '0', 
-        }), 
-        columnHelper.accessor('Prod_validade', {
-            header: () => 'Validade',
-            cell: info => `${info.getValue()}`, 
-        }),
-        columnHelper.accessor('Prod_preco', {
-            header: () => 'Preço',
-            cell: info => `${info.getValue()}`, 
-        }),
-        columnHelper.accessor('Prod_status', {
-            header: () => <div className="th-center"> Status</div>,
-            cell: info => (
-              <div className="td-center">
-                <ToggleBtn
-                  checked={info.getValue() == 1} 
-                  cod={info.row.original.Prod_cod}
-                  onStatusChange={(newStatus:any) => handleStatusChange(info.row.original.Prod_cod, newStatus)} 
-                />
-              </div>
-            ),
-        }),
-        columnHelper.display({
-          id: 'actions',
-          cell: props => <EditarRemoverBtn />
-      }),
-    ]
+        }), */
+    columnHelper.accessor('Prod_validade', {
+      header: () => 'Validade',
+      cell: info => `${info.getValue()}`,
+    }),
+    columnHelper.accessor('Prod_preco', {
+      header: () => 'Preço',
+      cell: info => `${info.getValue()}`,
+    }),
+    columnHelper.accessor('Prod_status', {
+      header: () => <div className="th-center"> Status</div>,
+      cell: info => (
+        <div className="td-center">
+          <ToggleBtn
+            checked={info.getValue() == 1}
+            cod={info.row.original.Prod_cod}
+            onStatusChange={(newStatus: any) => handleStatusChange(info.row.original.Prod_cod, newStatus)}
+          />
+        </div>
+      ),
+    }),
+    columnHelper.display({
+      id: 'actions',
+      cell: props => <EditarRemoverBtn />
+    }),
+  ]
 
   // Cria a tabela, aqui é onde serão passados todos os possíveis parâmetros
   const table = useReactTable({
@@ -99,16 +117,20 @@ function Produtos() {
     getCoreRowModel: getCoreRowModel(),
   })
 
-    return (
-      <main>
-        <div className="page-title">
-          <h1 className="title">Produtos</h1>
-          <hr className="line"/>
-        </div>
+  return (
+    <main>
+      <div className="page-title">
+        <h1 className="title">Produtos</h1>
+        <hr className="line" />
+      </div>
 
-        <Table hover responsive size="lg">
+      <div className="actions-group">
+        <BtnAzul className="rfloat" icon={<IoAddCircleOutline />} label="CADASTRAR" onClick={() => setOpenModalCadastro(true)} />
+      </div>
+
+      <Table hover responsive size="lg">
         <thead>
-        {table.getHeaderGroups().map(headerGroup => ( // Mapeia o cabeçalho (th) de cada coluna
+          {table.getHeaderGroups().map(headerGroup => ( // Mapeia o cabeçalho (th) de cada coluna
             <tr key={headerGroup.id}>
               {headerGroup.headers.map(header => (
                 <th key={header.id}>
@@ -126,17 +148,47 @@ function Produtos() {
               <tr className="table-row">
                 {row.getVisibleCells().map(cell => (
                   <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())} 
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </td>
                 ))}
               </tr>
-              <tr className="vazia"></tr>   
+              <tr className="vazia"></tr>
             </React.Fragment>
           ))}
         </tbody>
       </Table>
+
+
+      {/* MODALS*/}
+      {/* Modal de Cadastro */}
+      <Modal
+        isOpen={openModalCadastro} // Abre o modal
+        label="Cadastrar Produto" // Titulo do modal
+        setModalOpen={() => setOpenModalCadastro(false)} // Função para fechar dentro do modal
+        buttons={
+          <>
+            <BtnCancelar onClick={() => setOpenModalCadastro(false)} /> {/*Fechar o modal */}
+            <BtnAzul
+              icon={<IoAddCircleOutline />}
+              label="CADASTRAR"
+              onClick={() => formRef.current?.submitForm()} /* Passa a função da referencia do formulario para poder enviar o submit */
+            />
+          </>
+        }
+      >
+        <ProdutoFormulario
+          ref={formRef} /* Passa a referencia do formulario */
+          onSuccess={message => {
+            setMensagemSucesso(message)
+            setOpenModalCadastro(false)
+            fetchProdutos() // Atualiza a tabela
+          }}
+        />
+      </Modal>
+
+
     </main>
-    )
+  )
 }
 
 export default Produtos
