aae7ff1169d88f3dd5bcf7036b6cd947004bcf1e
Eric Lourenco <ericloumendes@gmail.com>
2024-11-04 19:31:40 -0300

    feat(back): Atualizando o model de categorias para ter um auto-relaciomento (#69)

--- backend/src/controllers/categoriaController.ts
+++ backend/src/controllers/categoriaController.ts
@@ -5,23 +5,23 @@ import { Produto } from '../models/Produto';
 export const controllerCategoria = {
   // POST /categoria
   save: async (req: Request, res: Response) => {
-    try {
-      const categoria = await Categoria.create(req.body);
-      return res.status(201).json(categoria);
-    } catch (error) {
-      return res.status(400).json({ error: 'Error saving Categoria', details: error.message });
-    }
+    // try {
+    //   const categoria = await Categoria.create(req.body);
+    //   return res.status(201).json(categoria);
+    // } catch (error) {
+    //   return res.status(400).json({ error: 'Error saving Categoria', details: error.message });
+    // }
   },
 
   // GET /categoria
   show: async (req: Request, res: Response) => {
-    try {
-      const categorias = await Categoria.findAll({
-        include: [Produto] // Incluindo produtos associados
-      });
-      return res.status(200).json(categorias);
-    } catch (error) {
-      return res.status(400).json({ error: 'Error fetching Categoria', details: error.message });
-    }
+  //   try {
+  //     const categorias = await Categoria.findAll({
+  //       include: [Produto] // Incluindo produtos associados
+  //     });
+  //     return res.status(200).json(categorias);
+  //   } catch (error) {
+  //     return res.status(400).json({ error: 'Error fetching Categoria', details: error.message });
+  //   }
   }
 }
--- backend/src/routes/categoriaRoutes.ts
+++ backend/src/routes/categoriaRoutes.ts
@@ -4,6 +4,13 @@ const routes = Router();
 
 // Rota para Categoria
 routes.post('/', controllerCategoria.save); // Criar categoria
+
 routes.get('/', controllerCategoria.show);  // Listar categorias
+
+routes.get('/:id', controllerCategoria.show); // Listar categorias com pai de id = ?
+
+routes.put('/', controllerCategoria.save); // Editar uma categoria
+
+routes.put('/status/:id', controllerCategoria.show); // Rota para mudar status do produto
 
 export default routes;
