0614d2aefebc6225e6b8b9f04756803176ae848d
Eric Lourenco <ericloumendes@gmail.com>
2024-11-08 18:36:08 -0300

    fix(front): Ajustando variavel de conexão ao buscar imagens

--- frontend/src/components/ImagemProduto/index.tsx
+++ frontend/src/components/ImagemProduto/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { hostname } from "../../config/apiConfig";
 
 interface Props {
     prod_cod: number
@@ -7,7 +8,7 @@ interface Props {
 const ImagemProduto: React.FC<Props> = ({ prod_cod }) => {
     return (
         <img
-          src={`http://localhost:5000/produto/DownloadImage/${prod_cod}`}
+          src={`${hostname}produto/DownloadImage/${prod_cod}`}
           alt="Produto"
           onError={(e) => {
             (e.currentTarget as HTMLImageElement).src = 'https://i.ibb.co/hYHgC6Z/sem-jpg.gif';
